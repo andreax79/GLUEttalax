86a95ee9d8270f328a41f7b35277a6cbaa53f0ee
Andrea Bonomi <andrea.bonomi@italiaonline.it>
2019-05-30 17:33:23 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+import codecs
+from setuptools import setup
+from freakotp import __version__
+
+d = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(d, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'freakotp',
+    version = __version__,
+    description = 'Glue ETL without constipation',
+    long_description = long_description,
+    url = 'https://github.com/andreax79/GLUEttalax',
+    author = 'Andrea Bonomi',
+    author_email = 'andrea.bonomi@gmail.com',
+    license = 'MIT',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    zip_safe=True,
+    include_package_data=True,
+    keywords = 'aws glue cli',
+    py_modules=[ 'gluettalax' ],
+    install_requires = [],
+    entry_points = {
+        'console_scripts': [
+            'gluettalax=gluettalax:main',
+            ],
+        }
+    )
